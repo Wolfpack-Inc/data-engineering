e9d203800a34f147b645829d84b5a8ed3eb8ce56
Jules Huisman <huisman.jules@gmail.com>
2019-10-23 18:07:38 +0200

    Created the twitter ingestion file

--- ingestion/twitter-ingestion.py
+++ ingestion/twitter-ingestion.py
@@ -1,7 +1,18 @@
+import pprint
+import json
+from tweepy import OAuthHandler, Stream, StreamListener
 from time import sleep
 from json import dumps, loads
 from kafka import KafkaProducer
+import tweepy
 
+# Twitter keys
+consumer_key = "I8Qvxs0xU5ifPHhfg98kxJVc4"
+consumer_secret = "43F9vXQIpDbJsB6TS8lVnKdBs7HjCpyLliM58oL8zCStAWD86J"
+access_token = "233923293-45rYG7fgTQb5HWJMY12YO32oXV6RlvQXFDsH5a0L"
+access_token_secret = "LMesjv8I1Ke85yVpRxFDblkVAOct7grAjnpc8uU8L9kjh"
+
+# Flag to check if kafka brokers are running
 kafka_running = False
 
 while kafka_running == False:
@@ -15,7 +26,22 @@ while kafka_running == False:
         kafka_running = True
         print("Connected to kafka broker!")
 
-# Send hello world message every 10 seconds
-while True:
-    producer.send('twitter', value={'hello': 'world'})
-    sleep(10)
+class StreamToKafka(StreamListener):
+    """ 
+    A listener handles tweets that are received from the stream.
+    """
+
+    def on_data(self, data):
+        tweet = json.loads(data)
+        producer.send('twitter', value=tweet)
+        return True
+
+    def on_error(self, status):
+        print(status)
+
+to_kafka = StreamToKafka()
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+stream = Stream(auth, to_kafka)
+stream.filter(track=['basketball'])
