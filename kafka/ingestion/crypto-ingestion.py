376341599cfba53e4ac070ffd04a0fe49d277354
Jules Huisman <huisman.jules@gmail.com>
2019-11-23 09:42:01 +0100

    Changed kafka server ip in ingestion

--- kafka/ingestion/crypto-ingestion.py
+++ kafka/ingestion/crypto-ingestion.py
@@ -12,7 +12,7 @@ kafka_running = False
 
 while kafka_running == False:
     try:
-        producer = KafkaProducer(bootstrap_servers=['kafka:9092'],
+        producer = KafkaProducer(bootstrap_servers=['kafka-1:9092'],
                                 value_serializer=lambda x: dumps(x).encode('utf-8'))
     except:
         print('No kafka brokers are running yet')
