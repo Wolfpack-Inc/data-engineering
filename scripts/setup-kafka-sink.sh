fb03146c00462ad97e6546c70edeead47e926985
Jules Huisman <huisman.jules@gmail.com>
2019-11-24 11:59:20 +0100

    -

--- scripts/setup-kafka-sink.sh
+++ scripts/setup-kafka-sink.sh
@@ -1,4 +1,4 @@
 curl --header "Content-Type: application/json" --request PUT --data "$(cat /scripts/twitter-index.json)" http://elasticsearch:9200/twitter
 curl --header "Content-Type: application/json" --request PUT --data "$(cat /scripts/crypto-index.json)" http://elasticsearch:9200/crypto
-curl --header "Content-Type: application/json" --request PUT --data "$(cat /scripts/prediction-index.json)" http://elasticsearch:9200/prediction
+curl --header "Content-Type: application/json" --request PUT --data "$(cat /scripts/prediction-index.json)" http://elasticsearch:9200/crypto-prediction
 curl --header "Content-Type: application/json" --request POST --data "$(cat /scripts/connector-setup.json)" http://localhost:8083/connectors
